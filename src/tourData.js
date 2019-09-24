5b6ac4813726bb76b006c870aace5b417c4458f1
Shan Fuyang <fuyang@Shans-MBP.attlocal.net>
2019-09-24 10:49:13 -0700

    Architectural Tour Project

---
+++ src/tourData.js
@@ -0,0 +1,50 @@
+export const tourData = [
+  {
+    id: 1,
+    city: "Finnland",
+    img: "./img/Finnland.jpg",
+    name: "Finnland - Alvar Aalto Tour",
+    info:
+      "Paimio Sanatorium is a former tuberculosis sanatorium in Paimio, Southwest Finland, designed by Finnish architect Alvar Aalto. The building was completed in 1932, and soon after received critical acclaim both in Finland and abroad. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 2,
+    city: "India",
+    img: "./img/India.jpg",
+    name: "paris lights tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 3,
+    city: "Japan",
+    img: "./img/Japan.jpg",
+    name: "Tadao Anto / Kenzo Tang / oshio Taniguchi / SANAA",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 4,
+    city: "Sri Lanka",
+    img: "./img/Sri Lanka.jpg",
+    name: "Geoffrey Bawa​",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 5,
+    city: "Bangladesh",
+    img: "./img/Bangladesh.jpg",
+    name: "tokyo sushi tour",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  },
+  {
+    id: 6,
+    city: "Sweden",
+    img: "./img/Sweden.jpg",
+    name: "Woodland cemetery - Asplund Erik Gnnar/ Sigurd Lewerentz",
+    info:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, nam omnis error corrupti eum assumenda enim odit architecto corporis. Sequi"
+  }
+];
